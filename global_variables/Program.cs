8d8559b6a1ff4f3fd21ff755c7063100fdcd0980
g.rithvik <g.rithvik909@gmail.com>
2024-06-21 09:50:47 +0530

    global variables

---
+++ global_variables/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace GlobalVariablesExample
+{
+    public static class Globals
+    {
+        public static int global1 = 0;
+        public static string Global2 = "Hello, World!";
+    }
+}
+
+namespace GlobalVariablesExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Initial Global Counter: " + Globals.global1);
+            Console.WriteLine("Initial Global Message: " + Globals.Global2);
+
+            Globals.global1++;
+            Globals.Global2 = "Hello, C#!";
+
+            Console.WriteLine("Updated Global Counter: " + Globals.global1);
+            Console.WriteLine("Updated Global Message: " + Globals.Global2);
+
+            AnotherClass.DoSomething();
+
+            Console.WriteLine("Final Global Counter: " + Globals.global1);
+        }
+    }
+
+    public class AnotherClass
+    {
+        public static void DoSomething()
+        {
+            Console.WriteLine("AnotherClass accessing Global Counter: " + Globals.global1);
+            Globals.global1 += 10;
+            Console.WriteLine("AnotherClass modified Global Counter: " + Globals.global1);
+        }
+    }
+}
